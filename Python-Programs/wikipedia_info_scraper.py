319380f37222ab8ee39351281092c094e9558061
Shubham-Jha99 <69886170+Shubham-Jha99@users.noreply.github.com>
2021-10-03 15:22:22 +0545

    Create wikipedia_info_scraper.py

---
+++ Python-Programs/wikipedia_info_scraper.py
@@ -0,0 +1,21 @@
+import wikipedia
+import cgi
+def que():
+    person = input('Write the title of the information you want to know = ')
+    info = wikipedia.summary(person, 2)
+    print(info)
+    print()
+    a = input("Want to get more information?(Y/N)")
+    if a == "y" or a == "Y":
+        info = wikipedia.summary(person)
+        print(info)
+        print()
+        print("SOURCE: WIKIPEDIA")
+        print()
+    elif a == "n" or a == "N":
+        print("Thanks For using us. Bye Bye.....")
+    else:
+        print("Invalid choice")
+
+while True:
+    que()
