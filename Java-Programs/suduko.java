f9fdeeb80fafa0d7e7895b3f4e031047069a48aa
Ravi Kumar Prasad <ravikrp22@gmail.com>
2021-10-03 12:09:52 +0530

    Update suduko.java

--- Java-Programs/suduko.java
+++ Java-Programs/suduko.java
@@ -38,13 +38,11 @@ public class suduko {
 		for (int x = 0; x <= 8; x++)
 			if (grid[x][col] == num)
 				return false;
-		int startRow = row - row % 3, startCol
-									= col - col % 3;
+		int startRow = row - row % 3, startCol= col - col % 3;
 		for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 3; j++)
 				if (grid[i + startRow][j + startCol] == num)
 					return false;
-
 		return true;
 	}
 
@@ -52,20 +50,20 @@ public class suduko {
 	public static void main(String[] args)
 	{
 		int grid[][] = { 
-						{ 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-						{ 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-						{ 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-						{ 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-						{ 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-						{ 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-						{ 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-						{ 0, 0, 0, 4, 1, 9, 0, 0, 5 },						
-						{ 0, 0, 0, 0, 8, 0, 0, 7, 9 },
-						};
+				{ 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+				{ 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+				{ 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+				{ 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+				{ 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+				{ 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+				{ 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+				{ 0, 0, 0, 4, 1, 9, 0, 0, 5 },						
+				{ 0, 0, 0, 0, 8, 0, 0, 7, 9 },
+				};
 
 		if (solveSuduko(grid, 0, 0))
 			print(grid);
 		else
-			System.out.println("No Solution exists");
+		System.out.println("No Solution exists");
 	}
 }
