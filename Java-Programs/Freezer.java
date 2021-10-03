f2597408c65371a1d903c1e0645f3847787beee3
Yashraj Prakash <53615477+YashrajPrakash@users.noreply.github.com>
2021-10-03 12:29:11 +0530

    Update Freezer.java

--- Java-Programs/Freezer.java
+++ Java-Programs/Freezer.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 import java.text.*;
 public class Freezer {
 public static void main(String[] args) {
-	NumberFormat nf=NumberFormat.getInstance();
-	Scanner sc=new Scanner(System.in);
-	int t1;
-	float Temp, t2;
+    NumberFormat nf=NumberFormat.getInstance();
+    Scanner sc=new Scanner(System.in);
+    int t1;
+    float Temp, t2;
     System.out.println("Enter Time since power failure..! ");
     System.out.println("Enter Hours: ");
-	t1=sc.nextInt();
+    t1=sc.nextInt();
     System.out.println("Enter Minutes: ");
-	t2=sc.nextFloat();
+    t2=sc.nextFloat();
     sc.close();
     t2=t2/60;
     float hours=t1+t2;
@@ -18,6 +18,5 @@ public static void main(String[] args) {
     System.out.println("Total time elapsed since power failure " +nf.format(hours)+"hrs");
     Temp=(4*(hours*hours))/(hours+2)-20;
     System.out.println("Current temperature of the freezer = " + Temp +" Celsius");
-	}
-
+  }
 }
