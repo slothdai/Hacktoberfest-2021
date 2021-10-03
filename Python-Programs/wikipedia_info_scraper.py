ead6a2e8e6a54df19e811e0b1f352890dd845af9
Shubham-Jha99 <69886170+Shubham-Jha99@users.noreply.github.com>
2021-10-03 16:16:03 +0545

    Update wikipedia_info_scraper.py

--- Python-Programs/wikipedia_info_scraper.py
+++ Python-Programs/wikipedia_info_scraper.py
@@ -1,18 +1,18 @@
 import wikipedia
 import cgi
 def que():
-    person = input('Write the title of the information you want to know = ')
-    info = wikipedia.summary(person, 2)
+    query = input('Write the title of the information you want to know = ')
+    info = wikipedia.summary(query, 2)
     print(info)
     print()
-    a = input("Want to get more information?(Y/N)")
-    if a == "y" or a == "Y":
-        info = wikipedia.summary(person)
+    answer = input("Want to get more information?(Y/N)")
+    if answer == "y" or answer == "Y":
+        info = wikipedia.summary(query)
         print(info)
         print()
         print("SOURCE: WIKIPEDIA")
         print()
-    elif a == "n" or a == "N":
+    elif answer == "n" or answer == "N":
         print("Thanks For using us. Bye Bye.....")
     else:
         print("Invalid choice")
